46f5f0aa174779008f7387f9de81e837058f4be2
peixin-github <Admin@AFAAW-704171443>
2018-02-24 19:21:58 +0800

    update

--- src/com/neuedu/com/Test.java
+++ src/com/neuedu/com/Test.java
@@ -8,45 +8,48 @@ import java.sql.SQLException;
 
 public class Test {
 	public static void main(String[] args) {
-		//geshi  jdbc:mysql://主机ip：端口号/数据库名
-		String url="jdbc:mysql://192.168.122.130:3306/db2?useUnicode=true&characterEncoding=utf8";
+		
+		//2.1格式jdbc:mysql://主机IP:端口号//数据库名
+		String url="jdbc:mysql://192.168.122.130:3306/db2";
 		String user="root";
 		String password="123456";
-		ResultSet rs=null;
+		//2.2创建必要对象,扩大作用域
+		Connection con = null;
 		PreparedStatement pstmt=null;
-		Connection con=null;
+		ResultSet rs=null;
 		try {
-			//选择要操作的数据库种类---加载驱动(反射)
-			Class.forName("com.mysql.jdbc.Driver");		
-			con= DriverManager.getConnection(url, user, password);
-			//创建命令窗口
-			pstmt=con.prepareStatement("select * from student");
-			//运行sql语句并查看结果，如果是增删改，则返回受影响的行数，如果是查询返回的是查询结果
-			//如果是增删改，则调用executeUpdate 返回int 如果查询则调用execute
-			rs= pstmt.executeQuery();
-			//查看结果集
+			//1.选择要创建的数据库----加载驱动(反射)
+			Class.forName("com.mysql.jdbc.Driver");
+			//2.创建连接 主机IP 端口号 用户名 密码 和选择要操作的数据库,url里包含主机IP、端口号和要操作的数据库
+			con = DriverManager.getConnection(url,user,password);
+			//3.创建命令窗口 写sql语句
+			pstmt = con.prepareStatement("select * from student");
+			//4.运行sql语句并查看结果 如果是增删改 返回受影响的行数 如果是查询返回的是查询结果
+			//4.1运行的时候 如果是增删改则调用executeUpdate 返回int(代表的是受影响的行数) 如果是查询 则调用executeQuery 返回ResultSet(结果的集合)
+			rs=pstmt.executeQuery();
 			while(rs.next()){
-				System.out.println(rs.getString("sno")+" "+rs.getString("sname"));				
+				System.out.println(rs.getString("sno")+"  "+rs.getString("sname"));
 			}
 		} catch (ClassNotFoundException e) {
-			// TODO: handle exception
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally{
+		}finally {
 			try {
-				if(rs!=null)
-					rs.close();
+				//空指针调用任何方法都会报错
+				if(rs!=null);
+				rs.close();
 				if(pstmt!=null)
-					pstmt.close();
+				pstmt.close();
 				if(con!=null)
-					con.close();
+				con.close();			
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
 		}
+		
 	}
 }
